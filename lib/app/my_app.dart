b671d7dccc8edef71e595a25be579df04ef09d32
Mahdiyar Arbabzi <m.arbabzi@gmail.com>
2022-10-05 21:00:50 +0330

    Create assets_manager & implement splash

--- lib/app/my_app.dart
+++ lib/app/my_app.dart
@@ -20,6 +20,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       onGenerateRoute: RouteGenerator.getRoute,
       initialRoute: Routes.splashRoute,
       theme: getApplicationTheme(),
---
+++ lib/presentation/resources/assets_manager.dart
@@ -0,0 +1,5 @@
+const String imagePath = 'assets/images';
+
+class ImageAssets {
+  static const String splashLogo = '$imagePath/icon/splash_logo.png';
+}
